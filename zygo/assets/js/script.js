6063c35977ea8c5912e974174649832953468594
sergiejziom <128434748+sergiejziom@users.noreply.github.com>
2023-04-12 21:23:04 +0200

    Add files via upload

---
+++ zygo/assets/js/script.js
@@ -0,0 +1,109 @@
+let mobile = 'ontouchstart' in document.documentElement;
+
+let switchAllowed = false;
+
+function openSocial(type) {
+  let url = 'about:blank';
+
+  switch (type) {
+    case 'discord':
+      url = 'https://discord.gg/w6kHZyTa9Q';
+      break;
+    case 'youtube':
+      url = 'https://www.youtube.com/@Zygo0001';
+      break;
+    case 'twitter':
+      url = 'https://steamcommunity.com/profiles/76561199225444119/';
+      break;
+      case 'twitch':
+        url = 'https://www.twitch.tv/zygo_only';
+        break;
+  }
+
+  window.open(url);
+}
+
+function startIntroTyping() {
+  new TypeIt('#intro-text', {
+    speed: 50,
+  })
+    .type('Hello.', { delay: 1200 })
+    .delete(null, { delay: 1000 })
+    .type(`${mobile ? 'tap' : 'press any key'} to enter.`)
+    .go();
+
+  setTimeout(function () {
+    switchAllowed = true;
+  }, 2500);
+}
+
+function typerStartTyping(typer) {
+  typer.reset();
+
+  let text = ['Najlepsze Citizeny Tylko na Fivem Hub Zapraszam'];
+
+  text.forEach(function (language, index) {
+    typer.move(null);
+    typer.type(language, { delay: 1000 });
+    typer.pause(1000);
+
+    typer.delete(language.length, { delay: 1000 });
+  });
+
+  typer.go();
+}
+
+function startMainTyping() {
+  let typer = new TypeIt('#subtext', {
+    speed: 50,
+    afterComplete: async () => {
+      typerStartTyping(typer);
+    },
+  });
+
+  typerStartTyping(typer);
+}
+
+function switchScreen() {
+  document.title = 'zygo.fun | home';
+
+  $('.intro').fadeOut(1000, function () {
+    $('.bg-image').fadeIn(1000);
+    $('.main').fadeIn(1000, function () {
+      startMainTyping();
+    });
+  });
+
+  ['background'].forEach(function (audioName) {
+    let fullPath = `assets/audio/${audioName}.mp3`;
+
+    let audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', fullPath);
+    audioElement.style.display = 'none';
+
+    audioElement.addEventListener('ended', function () {
+      this.currentTime = 0;
+      this.play();
+    });
+
+    audioElement.play();
+  });
+}
+
+document.addEventListener('keydown', function (e) {
+  if (switchAllowed) {
+    switchAllowed = false;
+    switchScreen();
+  }
+});
+
+document.addEventListener('touchstart', function (e) {
+  if (switchAllowed && mobile) {
+    switchAllowed = false;
+    switchScreen();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  startIntroTyping();
+});
